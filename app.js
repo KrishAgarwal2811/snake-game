32bdfde45d8be4beccd4b5eb6a94570fedeb0a12
KrishAgarwal2811 <krishagarwal2811@gmail.com>
2021-04-21 19:09:17 +0530

    Fixed many bugs & added some styling to the snake & added event listeners to control buttons

--- app.js
+++ app.js
@@ -7,81 +7,9 @@ let running = false;
 let apple;
 let frames = 0;
 
-class Snake {
-  constructor() {
-    this.x = Math.round(Math.random() * (canvas.width / scl)) * scl;
-    this.y = Math.round(Math.random() * (canvas.height / scl)) * scl;
-
-    this.velocityX = scl;
-    this.velocityY = 0;
-    this.tail = 0;
-
-    this.fullSnake = [];
-  }
-
-  draw() {
-    ctx.fillStyle = "white";
-    ctx.fillRect(this.x, this.y, scl, scl);
-  }
-
-  dir(velX, velY) {
-    if (velX != this.velocityX && velY != this.velocityY) {
-      this.velocityX = velX;
-      this.velocityY = velY;
-    } else {
-      console.log("Same ");
-    }
-  }
-
-  eatFood(food) {
-    if (this.x == food.x && this.y == food.y) {
-      this.tail++;
-      // console.log(this.tail);
-
-      this.fullSnake.push({
-        x: this.x,
-        y: this.y,
-      });
-
-      newFood();
-      drawFood(apple);
-    }
-  }
-
-  update() {
-    clearCanvas();
-    for (let i = 0; i < this.tail; i++) {
-      ctx.fillStyle = "white";
-      ctx.fillRect(this.fullSnake[i].x, this.fullSnake[i].y, scl, scl);
-
-      this.fullSnake[i] = this.fullSnake[i + 1];
-
-      if (this.x == this.fullSnake[i].x && this.y == this.fullSnake[i].y) {
-        alert("Game Over");
-        this.tail = 0;
-        this.fullSnake = [];
-        window.cancelAnimationFrame(raf);
-        return;
-      }
-    }
-    this.x += this.velocityX;
-    this.y += this.velocityY;
-
-    if (this.x >= canvas.width) this.x = 0;
-    if (this.x < 0) this.x = canvas.width - scl;
-    if (this.y > canvas.height) this.y = 0;
-    if (this.y < 0) this.y = canvas.height;
-
-    this.fullSnake[this.tail - 1] = {
-      x: this.x,
-      y: this.y,
-    };
-
-    this.eatFood(apple);
-    drawFood(apple);
-
-    this.draw();
-  }
+// All indipendent fucntions lie here
+function dist(x1, x2, y1, y2) {
+  return Math.hypot(x1 - x2, y1 - y2);
 }
 
 function drawFood(food) {
@@ -93,36 +21,130 @@ function randPoint(size) {
   return Math.round(Math.random() * (size / scl - 1)) * scl;
 }
 
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+// The main snake class
+class Snake {
+  constructor() {
+    this.x = Math.round(Math.random() * (canvas.width / scl)) * scl;
+    this.y = Math.round(Math.random() * (canvas.height / scl)) * scl;
+
+    this.velocityX = scl;
+    this.velocityY = 0;
+
+    this.fullSnake = [];
+    this.tail = 0;
+  }
+
+  draw() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(this.x + 1, this.y + 1, scl - 2, scl - 2);
+    ctx.strokeStyle = "#616161";
+    ctx.strokeRect(this.x, this.y, scl, scl);
+  }
+
+  dir(velX, velY) {
+    if (velX != this.velocityX && velY != this.velocityY) {
+      this.velocityX = velX;
+      this.velocityY = velY;
+    }
+  }
+
+  eatFood(food) {
+    if (this.x == food.x && this.y == food.y) {
+      for (let i = this.tail; i > 0; i--) {
+        this.fullSnake[i] = this.fullSnake[i - 1];
+      }
+
+      this.fullSnake[0] = {
+        x: this.x,
+        y: this.y,
+      };
+
+      this.tail++;
+
+      newFood();
+      drawFood(apple);
+    }
+  }
+
+  death() {
+    for (let i = 0; i < this.tail; i++) {
+      let spot = this.fullSnake[i];
+      let d = dist(this.x, spot.x, this.y, spot.y);
+      if (d < 1) {
+        this.fullSnake = [];
+        this.tail = 0;
+        alert("Game Over");
+        running = false;
+        clearCanvas();
+      }
+    }
+  }
+
+  update() {
+    clearCanvas();
+
+    this.x += this.velocityX;
+    this.y += this.velocityY;
+
+    this.death();
+    this.eatFood(apple);
+    drawFood(apple);
+
+    for (let i = 0; i < this.tail; i++) {
+      ctx.fillStyle = "white";
+      ctx.fillRect(
+        this.fullSnake[i].x + 1,
+        this.fullSnake[i].y + 1,
+        scl - 2,
+        scl - 2
+      );
+      ctx.strokeStyle = "#616161";
+      ctx.strokeRect(this.fullSnake[i].x, this.fullSnake[i].y, scl, scl);
+      this.fullSnake[i] = this.fullSnake[i + 1];
+    }
+
+    if (this.x >= canvas.width) this.x = 0;
+    if (this.x < 0) this.x = canvas.width - scl;
+    if (this.y >= canvas.height) this.y = 0;
+    if (this.y < 0) this.y = canvas.height;
+
+    this.fullSnake[this.fullSnake.length - 1] = {
+      x: this.x,
+      y: this.y,
+    };
+
+    this.draw();
+  }
+}
+
+// All depended functions lie here
 function newFood() {
   let appleX = randPoint(canvas.width);
   let appleY = randPoint(canvas.height);
 
-  for (let i = 0; i < mySnake.tail; ) {
+  for (let i = 0; i < mySnake.tail; i++) {
     let block = mySnake.fullSnake[i];
 
     if (block.x == appleX && block.y == apple.Y) {
       appleX = randPoint(canvas.width);
       appleY = randPoint(canvas.height);
       i = -1;
-      console.log("Tail");
-    } else if (mySnake.x == appleX && mySnake.y == appleY) {
-      appleX = randPoint(canvas.width);
-      appleY = randPoint(canvas.height);
-      i = -1;
-      console.log("Head");
     }
+  }
 
-    i++;
+  if (mySnake.x == appleX && mySnake.y == appleY) {
+    appleX = randPoint(canvas.width);
+    appleY = randPoint(canvas.height);
   }
 
   apple = {
     x: appleX,
     y: appleY,
   };
-}
-
-function clearCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function keyPressed(event) {
@@ -155,7 +177,7 @@ function keyPressed(event) {
 function update() {
   frames++;
 
-  if (frames >= 20) {
+  if (frames >= 10) {
     mySnake.update();
     frames = 0;
   }
@@ -165,21 +187,21 @@ function update() {
 
 function clicked() {
   clearCanvas();
-  mySnake.fullSnake.push({
-    x: mySnake.x,
-    y: mySnake.y,
-  });
-  // mySnake.update();
+  mySnake.eatFood({ x: mySnake.x, y: mySnake.y });
+  mySnake.update();
 }
 
 function draw() {
   canvas = document.querySelector("canvas");
   ctx = canvas.getContext("2d");
 
-  canvas.width = 660;
-  canvas.height = 500;
+  canvas.width = Math.floor(window.innerWidth / scl) * scl;
+  canvas.height =
+    Math.floor(
+      (window.innerHeight - document.querySelector("#controls").offsetHeight) /
+        scl
+    ) * scl;
   canvas.style.background = "#515151";
-  ctx.lineWidth = 2;
 
   mySnake = new Snake();
   newFood();
@@ -187,7 +209,13 @@ function draw() {
   mySnake.draw();
 
   document.body.addEventListener("keydown", keyPressed);
-  document.body.addEventListener("click", clicked);
+
+  document.querySelectorAll("#controls button").forEach((button) => {
+    button.addEventListener("click", () => {
+      keyPressed({ keyCode: parseInt(button.getAttribute("data-keyCode")) });
+    });
+  });
 }
 
 window.onload = draw;
+window.onresize = draw;
