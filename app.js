8202206337141a18bf6b5eeee6c73621c8fad9c6
KrishAgarwal2811 <krishagarwal2811@gmail.com>
2021-04-26 18:05:02 +0530

    Added support for ASWD and swipe controls.

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ let running = false;
 let apple;
 let frames = 0;
 
+alert("You can use arrow keys or ASWD or swipe to move the snake.");
+
 // All indipendent fucntions lie here
 function dist(x1, x2, y1, y2) {
   return Math.hypot(x1 - x2, y1 - y2);
@@ -24,6 +26,88 @@ function randPoint(size) {
 function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+/*
+ * :CREDITS:
+ *
+ * SWIPE DETECTOR V1.1
+ * Made By Good_Bits
+ * :)
+ *
+ * Thanks to this awesome guy :D
+ */
+
+let SwipeDetector = (function () {
+  let touchStart = null;
+  let lastTouch = null;
+  let timeout = 500;
+  let threshold = 50;
+  let eventHandlers = [];
+  let lastTime = NaN;
+  let disabled = false;
+
+  class vec2D {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    mag() {
+      return Math.sqrt(this.x ** 2 + this.y ** 2);
+    }
+  }
+
+  function vec2DSubtract(a, b) {
+    return new vec2D(a.x - b.x, a.y - b.y);
+  }
+
+  function handleTouchStart(event) {
+    touchStart = new vec2D(event.touches[0].clientX, event.touches[0].clientY);
+    lastTime = Date.now();
+  }
+
+  function handleTouchMove(event) {
+    lastTouch = new vec2D(event.touches[0].clientX, event.touches[0].clientY);
+  }
+
+  function handleTouchEnd(event) {
+    if (disabled === true) return;
+    lastTouch = lastTouch || touchStart;
+    let swipeVector = vec2DSubtract(touchStart, lastTouch);
+    let eventName = "";
+    let timeDiff = Date.now() - lastTime;
+    if (swipeVector.mag() < threshold || timeDiff > timeout) return;
+    if (Math.abs(swipeVector.x) > Math.abs(swipeVector.y)) {
+      eventName = swipeVector.x < 0 ? "swipeRight" : "swipeLeft";
+    } else {
+      eventName = swipeVector.y < 0 ? "swipeDown" : "swipeUp";
+    }
+
+    if (typeof eventHandlers[eventName] === "function") {
+      eventHandlers[eventName](touchStart, lastTouch);
+    }
+  }
+  window.addEventListener("touchstart", handleTouchStart);
+  window.addEventListener("touchmove", handleTouchMove);
+  window.addEventListener("touchend", handleTouchEnd);
+  return {
+    addEventListener: function (event, handler) {
+      eventHandlers[event] = handler;
+    },
+    setSwipeThreshold: function (value) {
+      threshold = value > 0 ? value : 50;
+    },
+    setSwipeTimeout: function (value) {
+      timeout = value > 0 ? value : 500;
+    },
+    disable: function () {
+      disabled = true;
+    },
+    enable: function () {
+      disabled = false;
+    },
+  };
+})();
 
 // The main snake class
 class Snake {
@@ -79,6 +163,7 @@ class Snake {
         this.tail = 0;
         alert("Game Over");
         running = false;
+        document.querySelector("#pause-btn").classList.toggle("paused");
         clearCanvas();
       }
     }
@@ -149,17 +234,22 @@ function newFood() {
 
 function keyPressed(event) {
   const key = event.keyCode;
+  // console.log(key);
 
   switch (key) {
+    case 65:
     case 37:
       mySnake.dir(-scl, 0);
       break;
+    case 87:
     case 38:
       mySnake.dir(0, -scl);
       break;
+    case 68:
     case 39:
       mySnake.dir(scl, 0);
       break;
+    case 83:
     case 40:
       mySnake.dir(0, scl);
       break;
@@ -170,6 +260,7 @@ function keyPressed(event) {
         raf = window.requestAnimationFrame(update);
       }
       running = !running;
+      document.querySelector("#pause-btn").classList.toggle("paused");
       break;
   }
 }
@@ -195,11 +286,12 @@ function draw() {
   canvas = document.querySelector("canvas");
   ctx = canvas.getContext("2d");
 
+  let pauseBtn = document.querySelector("#pause-btn");
+
   canvas.width = Math.floor(window.innerWidth / scl) * scl;
   canvas.height =
     Math.floor(
-      (window.innerHeight - document.querySelector("#controls").offsetHeight) /
-        scl
+      (window.innerHeight - pauseBtn.getBoundingClientRect().height) / scl
     ) * scl;
   canvas.style.background = "#515151";
 
@@ -210,10 +302,31 @@ function draw() {
 
   document.body.addEventListener("keydown", keyPressed);
 
-  document.querySelectorAll("#controls button").forEach((button) => {
-    button.addEventListener("click", () => {
-      keyPressed({ keyCode: parseInt(button.getAttribute("data-keyCode")) });
-    });
+  pauseBtn.addEventListener("click", function (event) {
+    if (running) {
+      window.cancelAnimationFrame(raf);
+    } else {
+      raf = window.requestAnimationFrame(update);
+    }
+    running = !running;
+    pauseBtn.classList.toggle("paused");
+  });
+
+  // Added swipe detection
+  SwipeDetector.addEventListener("swipeLeft", function () {
+    mySnake.dir(-scl, 0);
+  });
+
+  SwipeDetector.addEventListener("swipeRight", function () {
+    mySnake.dir(scl, 0);
+  });
+
+  SwipeDetector.addEventListener("swipeUp", function () {
+    mySnake.dir(0, -scl);
+  });
+
+  SwipeDetector.addEventListener("swipeDown", function () {
+    mySnake.dir(0, scl);
   });
 }
 
